3ebf2a410e285c79d6654b2d7f69fc225de6405c
khairulazanni <khairul.azanni@gmail.com>
2025-12-06 21:13:23 +0800

    feat: add theme

--- frontend/daily_quran/lib/main.dart
+++ frontend/daily_quran/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+// Constants import
+import 'package:daily_quran/core/constants/app_constant.dart';
+
 // Router import
 import 'package:daily_quran/app/router/AppRouter.dart';
 
@@ -25,6 +28,26 @@ class DailyQuranApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: AppRouter.router,
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          primary: AppConstants.primaryDark,
+          secondary: AppConstants.primaryMedium,
+          surface: AppConstants.primaryLight,
+          error: AppConstants.errorColor,
+          onSurface: AppConstants.textPrimary,
+        ),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.dark(
+          primary: AppConstants.primaryMedium,
+          secondary: AppConstants.primaryDark,
+          surface: AppConstants.primaryDarkest,
+          error: AppConstants.errorColor,
+          onSurface: AppConstants.textLight,
+        ),
+        useMaterial3: true,
+      ),
     );
   }
 }
--- frontend/daily_quran/lib/welcome_page.dart
+++ frontend/daily_quran/lib/welcome_page.dart
@@ -5,9 +5,11 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Welcome Page'),
+        child: Text(
+          'Welcome Page'
+          ),
       ),
     );
   }
